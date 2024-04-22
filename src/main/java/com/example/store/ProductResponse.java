296da4d3161400775820dc906c42c4e5a743cdc4
lhyeonnit <lhyeonnit@gmail.com>
2024-04-22 12:42:43 +0900

    ProductResponse UpdateDTO

--- src/main/java/com/example/store/ProductResponse.java
+++ src/main/java/com/example/store/ProductResponse.java
@@ -5,6 +5,21 @@ import lombok.Data;
 public class ProductResponse {
 
     @Data
+    public static class UpdateDTO{
+        private int id;
+        private String name;
+        private int price ;
+        private int qty;
+
+        public UpdateDTO(Product product){
+            this.id = product.getId();
+            this.name = product.getName();
+            this.price = product.getPrice();
+            this.qty = product.getQty();
+        }
+    }
+
+    @Data
     public static class SaveDTO{
         private int id;
         private String name;

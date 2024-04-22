fdb2c08f4d6a10b4859ef0807d970ab0cee48e7c
lhyeonnit <lhyeonnit@gmail.com>
2024-04-22 12:17:04 +0900

    ProductService getProductList

--- src/main/java/com/example/store/ProductService.java
+++ src/main/java/com/example/store/ProductService.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ProductService {
@@ -11,7 +13,9 @@ public class ProductService {
     private final EntityManager em;
 
     //상품 목록보기
-
+    public List<ProductResponse.ListDTO> getProductList(){
+        return productRepository.findAll();
+    }
     //상품 등록
 
     //상품 업데이트

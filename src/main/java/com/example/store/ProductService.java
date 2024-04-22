4aef8a58dc6eb1a4425d7c27113e20552ddf96ec
lhyeonnit <lhyeonnit@gmail.com>
2024-04-22 12:27:42 +0900

    ProductService  getProductDetail

--- src/main/java/com/example/store/ProductService.java
+++ src/main/java/com/example/store/ProductService.java
@@ -12,6 +12,10 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final EntityManager em;
 
+    //상품 상세 보기
+    public ProductResponse.DetailDTO getProductDetail(int id){
+        return productRepository.findById(id);
+    }
     //상품 목록보기
     public List<ProductResponse.ListDTO> getProductList(){
         return productRepository.findAll();

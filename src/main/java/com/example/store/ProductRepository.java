daf7f931d5a0e1b7304cbd9bfe77ec6c7fee264d
lhyeonnit <lhyeonnit@gmail.com>
2024-04-22 12:24:13 +0900

    ProductRepository findById

--- src/main/java/com/example/store/ProductRepository.java
+++ src/main/java/com/example/store/ProductRepository.java
@@ -12,6 +12,11 @@ import java.util.List;
 public class ProductRepository {
     private final EntityManager em;
 
+    public ProductResponse.DetailDTO findById(int id) {
+        Product product = em.find(Product.class, id);
+        return new ProductResponse.DetailDTO(product);
+    }
+
     public List<ProductResponse.ListDTO> findAll() {
         Query query =
                 em.createQuery("select p from Product p order by p.id desc", Product.class);
